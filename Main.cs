113a27c476e07bfb27dcd18fa89c843a26a1b7aa
Incorporado <incorporado@gmail.com>
2013-09-16 17:33:54 -0300

    agrego git ignore

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fiuba.Tecnicas.Giledrose
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
+
--- giledrose/Inventory.cs
+++ giledrose/Inventory.cs
@@ -106,43 +106,29 @@ namespace Fiuba.Tecnicas.Giledrose
         }*/
             foreach (var item in items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if ( (item.Name != "Aged Brie") && (item.Name != "Backstage passes to a TAFKAL80ETC concert") 
+                    && (item.Name != "Sulfuras, Hand of Ragnaros") && (item.Quality > 0) )
                 {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            item.Quality -= 1;
-                        }
-                    }
+                    item.Quality -= 1;
                 }
                 else
                 {
-                    if (item.Quality < 50)
+                    if ( (item.Name == "Backstage passes to a TAFKAL80ETC concert") && (item.Quality < 50) )
+                    {
+                        if (item.SellIn < 6)
+                        {
+                            item.Quality += 3;
+                        }
+                        else if (item.SellIn < 11)
+                        {
+                            item.Quality += 2;                                
+                        }                            
+                    }
+                    else 
                     {
                         item.Quality += 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.SellIn < 11)
-
-
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality += 1;
-                                }
-                            }
-                        }
                     }
+                    
                 }
 
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
