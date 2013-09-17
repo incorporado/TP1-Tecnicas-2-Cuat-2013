1f84f77aed17009f69ee47e7ccc8f41d64b6a6f0
Incorporado <incorporado@gmail.com>
2013-09-17 17:20:02 -0300

    tercer refactor por Consolidate duplicate conditional fragments

--- giledrose/Inventory.cs
+++ giledrose/Inventory.cs
@@ -32,62 +32,60 @@ namespace Fiuba.Tecnicas.Giledrose
             foreach (var item in items)
             {
                 if ( (item.Name != "Aged Brie") && (item.Name != "Backstage passes to a TAFKAL80ETC concert") 
-                    && (item.Name != "Sulfuras, Hand of Ragnaros") && (item.Quality > 0) )
-                {
-                    item.Quality -= 1;
-                }
-
-                if ( (item.Name == "Aged Brie")  && (item.Quality < 50) )
-                {
-                    item.Quality += 1;
-                }
-
-                if (item.Name == "Backstage passes to a TAFKAL80ETC concert") 
-                {
-                    item.Quality += 1;
-                }
-
-                if ( (item.Name == "Backstage passes to a TAFKAL80ETC concert") && (item.Quality < 50) )
-                {
-                    if (item.SellIn < 6)
-                    {
-                        item.Quality += 2;
-                    }
-                    else if (item.SellIn < 11)
-                    {
-                        item.Quality += 1;                                
-                    }                            
-                }                
-
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
+                    && (item.Name != "Sulfuras, Hand of Ragnaros") )
                 {
                     item.SellIn -= 1;
-                }
 
-                /******************/
+                    if ( (item.SellIn < 0) && (item.Quality - 1 > 0) )
+                    {
+                        item.Quality -= 2;
+                        
+                    }
+                    else if (item.Quality > 0)
+                    {
+                        item.Quality -= 1;                        
+                    }
+                }               
 
-                if (item.SellIn < 0)
+                if ( (item.Name == "Aged Brie") )
                 {
-                    if ((item.Name == "Aged Brie") && (item.Quality < 50))
+                    item.SellIn -= 1;   
+
+                    if ( (item.Quality + 1 < 50) && (item.SellIn < 0) )
+                    {
+                        item.Quality += 2;                                         
+                    }
+                    else if (item.Quality < 50)
                     {
                         item.Quality += 1;
                     }
-                    else {
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            item.Quality = 0;
-                        }
-                        else 
-                        {
-                            if ( (item.Quality > 0) && (item.Name != "Sulfuras, Hand of Ragnaros") )
-                            {                                
-                                {
-                                    item.Quality -= 1;
-                                }
-                            }
-                        }                        
-                    }                    
                 }
+
+                if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                {                    
+
+                    if (item.SellIn - 1 < 0)
+                    {
+                        item.Quality = 0;                        
+                    }
+                    else
+                    {                        
+                        if (item.Quality + 1 > 50 - 1)
+                        {
+                            item.Quality += 1;
+                        }
+                        else if ( (item.Quality + 1 < 50) && (item.SellIn < 6) ) 
+                        {                            
+                            item.Quality += 3;                            
+                        }
+                        else if ( (item.Quality + 1 < 50) && (item.SellIn < 11) ) 
+                        {
+                            item.Quality += 2;
+                        }                        
+                    }
+
+                    item.SellIn -= 1;
+                }               
             }
         }
     }
