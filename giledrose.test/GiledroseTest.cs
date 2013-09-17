505ffed1b292685245bc976d31e9055955ff3c72
Incorporado <incorporado@gmail.com>
2013-09-17 14:39:20 -0300

    arreglo tests, corrijo Inventory por bad smell Long Method y refactorizo por Consolidate duplicate conditional fragments

--- giledrose.test/GiledroseTest.cs
+++ giledrose.test/GiledroseTest.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using Fiuba.Tecnicas.Giledrose;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +11,16 @@ namespace Giledrose.Test
     [TestClass]
     public class GiledroseTest
     {
-        List<Item> items;
+        List<Item> itemsNuevos;
+        List<Item> itemsSinCambios;
+        
         Inventory inventario;
 
-        public GiledroseTest()
+        //public GiledroseTest()
+        [TestInitialize()]
+        public void setUp()
         {
-            this.items = new List<Item>
+            this.itemsNuevos = new List<Item>
             {
                 new Item("Sulfuras, Hand of Ragnaros", 0, 0), //1
                 new Item("Sulfuras, Hand of Ragnaros", -1, 0), //2
@@ -40,157 +43,150 @@ namespace Giledrose.Test
                 new Item("Conjured Mana Cake", 10, 5),//19
             };
 
-            this.inventario = new Inventory(items);
+            this.itemsSinCambios = new List<Item>
+            {
+                new Item("Sulfuras, Hand of Ragnaros", 0, 0), //1
+                new Item("Sulfuras, Hand of Ragnaros", -1, 0), //2
+                new Item("Sulfuras, Hand of Ragnaros", -1, 50),//3
+                new Item("Aged Brie", 50, 50),//4
+                new Item("Aged Brie", 0, 48),//5
+                new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50),//6
+                new Item("Backstage passes to a TAFKAL80ETC concert", 12, 49),//7
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),//8
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 48),//9
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47),//10
+                new Item("Backstage passes to a TAFKAL80ETC concert", -1, 46),//11
+                new Item("Backstage passes to a TAFKAL80ETC concert", -1, -10),//12
+                new Item("+5 Dexterity Vest", 1, 1),//13
+                new Item("+5 Dexterity Vest", 0, 1),//14
+                new Item("+5 Dexterity Vest", 0, 2),//15
+                new Item("Conjured Mana Cake", 0, 2),//16
+                new Item("Elixir of the Mongoose", 0, 10),//17
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80),//18
+                new Item("Conjured Mana Cake", 10, 5),//19
+            };
+            
+            this.inventario = new Inventory(itemsNuevos);
             this.inventario.updateQuality();
         }
 
         [TestMethod]
         public void TestParaElemento1DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[0].SellIn;
-            int valorAnteriorDeQuality = this.items[0].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn, this.items[0].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality, this.items[0].Quality);
+            Assert.AreEqual(this.itemsSinCambios[0].SellIn, this.itemsNuevos[0].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[0].Quality, this.itemsNuevos[0].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento2DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[1].SellIn;
-            int valorAnteriorDeQuality = this.items[1].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn, this.items[1].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality, this.items[1].Quality);
+            Assert.AreEqual(this.itemsSinCambios[1].SellIn, this.itemsNuevos[1].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[1].Quality, this.itemsNuevos[1].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento3DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[2].SellIn;
-            int valorAnteriorDeQuality = this.items[2].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn, this.items[2].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality, this.items[2].Quality);
+            Assert.AreEqual(this.itemsSinCambios[2].SellIn, this.itemsNuevos[2].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[2].Quality, this.itemsNuevos[2].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento4DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[3].SellIn;
-            int valorAnteriorDeQuality = this.items[3].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[3].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality, this.items[3].Quality);
+            Assert.AreEqual(this.itemsSinCambios[3].SellIn - 1, this.itemsNuevos[3].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[3].Quality, this.itemsNuevos[3].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento5DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[4].SellIn;
-            int valorAnteriorDeQuality = this.items[4].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[4].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality + 2, this.items[4].Quality);
+            Assert.AreEqual(this.itemsSinCambios[4].SellIn - 1, this.itemsNuevos[4].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[4].Quality + 2, this.itemsNuevos[4].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento6DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[5].SellIn;
-            int valorAnteriorDeQuality = this.items[5].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[5].SellIn);
-            Assert.AreEqual(0, this.items[5].Quality);
+            Assert.AreEqual(this.itemsSinCambios[5].SellIn - 1, this.itemsNuevos[5].SellIn);
+            Assert.AreEqual(0, this.itemsNuevos[5].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento7DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[6].SellIn;
-            int valorAnteriorDeQuality = this.items[6].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[6].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality + 1, this.items[6].Quality);
+            Assert.AreEqual(this.itemsSinCambios[6].SellIn - 1, this.itemsNuevos[6].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[6].Quality + 1, this.itemsNuevos[6].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento8DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[7].SellIn;
-            int valorAnteriorDeQuality = this.items[7].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[7].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality + 1, this.items[7].Quality);
+            Assert.AreEqual(this.itemsSinCambios[7].SellIn - 1, this.itemsNuevos[7].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[7].Quality + 1, this.itemsNuevos[7].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento9DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[8].SellIn;
-            int valorAnteriorDeQuality = this.items[8].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[8].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality + 2, this.items[8].Quality);
+            Assert.AreEqual(this.itemsSinCambios[8].SellIn - 1, this.itemsNuevos[8].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[8].Quality + 2, this.itemsNuevos[8].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento10DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[9].SellIn;
-            int valorAnteriorDeQuality = this.items[9].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[9].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality + 3, this.items[9].Quality);
+            Assert.AreEqual(this.itemsSinCambios[9].SellIn - 1, this.itemsNuevos[9].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[9].Quality + 3, this.itemsNuevos[9].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento11DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[10].SellIn;
-            int valorAnteriorDeQuality = this.items[10].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[10].SellIn);
-            Assert.AreEqual(0, this.items[10].Quality);
+            Assert.AreEqual(this.itemsSinCambios[10].SellIn - 1, this.itemsNuevos[10].SellIn);
+            Assert.AreEqual(0, this.itemsNuevos[10].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento12DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[11].SellIn;
-            int valorAnteriorDeQuality = this.items[11].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[11].SellIn);
-            Assert.AreEqual(0, this.items[11].Quality);
+            Assert.AreEqual(this.itemsSinCambios[11].SellIn - 1, this.itemsNuevos[11].SellIn);
+            Assert.AreEqual(0, this.itemsNuevos[11].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento13DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[12].SellIn;
-            int valorAnteriorDeQuality = this.items[12].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[12].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 1, this.items[12].Quality);
+            Assert.AreEqual(this.itemsSinCambios[12].SellIn - 1, this.itemsNuevos[12].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[12].Quality - 1, this.itemsNuevos[12].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento14DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[13].SellIn;
-            int valorAnteriorDeQuality = this.items[13].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[13].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 1, this.items[13].Quality);
+            Assert.AreEqual(this.itemsSinCambios[13].SellIn - 1, this.itemsNuevos[13].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[13].Quality - 1, this.itemsNuevos[13].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento15DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[14].SellIn;
-            int valorAnteriorDeQuality = this.items[14].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[14].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 2, this.items[14].Quality);
+            Assert.AreEqual(this.itemsSinCambios[14].SellIn - 1, this.itemsNuevos[14].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[14].Quality - 2, this.itemsNuevos[14].Quality);
 
         }
 
@@ -198,20 +194,16 @@ namespace Giledrose.Test
         [TestMethod]
         public void TestParaElemento16DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[15].SellIn;
-            int valorAnteriorDeQuality = this.items[15].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[15].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 2, this.items[15].Quality);
+            Assert.AreEqual(this.itemsSinCambios[15].SellIn - 1, this.itemsNuevos[15].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[15].Quality - 2, this.itemsNuevos[15].Quality);
 
         }
 
         [TestMethod]
         public void TestParaElemento17DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[16].SellIn;
-            int valorAnteriorDeQuality = this.items[16].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[16].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 2, this.items[16].Quality);
+            Assert.AreEqual(this.itemsSinCambios[16].SellIn - 1, this.itemsNuevos[16].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[16].Quality - 2, this.itemsNuevos[16].Quality);
 
         }
 
@@ -219,19 +211,15 @@ namespace Giledrose.Test
         [TestMethod]
         public void TestParaElemento18DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[17].SellIn;
-            int valorAnteriorDeQuality = this.items[17].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn, this.items[17].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality, this.items[17].Quality);
+            Assert.AreEqual(this.itemsSinCambios[17].SellIn, this.itemsNuevos[17].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[17].Quality, this.itemsNuevos[17].Quality);
         }
 
         [TestMethod]
         public void TestParaElemento19DeLaLista()
         {
-            int valorAnteriorDeSellIn = this.items[18].SellIn;
-            int valorAnteriorDeQuality = this.items[18].Quality;
-            Assert.AreEqual(valorAnteriorDeSellIn - 1, this.items[18].SellIn);
-            Assert.AreEqual(valorAnteriorDeQuality - 2, this.items[18].Quality);
+            Assert.AreEqual(this.itemsSinCambios[18].SellIn - 1, this.itemsNuevos[18].SellIn);
+            Assert.AreEqual(this.itemsSinCambios[18].Quality - 1, this.itemsNuevos[18].Quality);
         }  
     }
 }
--- giledrose/Inventory.cs
+++ giledrose/Inventory.cs
@@ -29,7 +29,7 @@ namespace Fiuba.Tecnicas.Giledrose
 
         public void updateQuality()
         {
-            /* foreach (var item in items)
+             /*foreach (var item in items)
             {
                 if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
@@ -50,8 +50,6 @@ namespace Fiuba.Tecnicas.Giledrose
                         if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (item.SellIn < 11)
-
-
                             {
                                 if (item.Quality < 50)
                                 {
@@ -111,26 +109,26 @@ namespace Fiuba.Tecnicas.Giledrose
                 {
                     item.Quality -= 1;
                 }
-                else
+
+                if ( ((item.Name == "Aged Brie") || (item.Name == "Backstage passes to a TAFKAL80ETC concert")) && (item.Quality < 50) )
                 {
-                    if ( (item.Name == "Backstage passes to a TAFKAL80ETC concert") && (item.Quality < 50) )
-                    {
-                        if (item.SellIn < 6)
-                        {
-                            item.Quality += 3;
-                        }
-                        else if (item.SellIn < 11)
-                        {
-                            item.Quality += 2;                                
-                        }                            
-                    }
-                    else 
-                    {
-                        item.Quality += 1;
-                    }
-                    
+                    item.Quality += 1;
                 }
 
+                if ( (item.Name == "Backstage passes to a TAFKAL80ETC concert") && (item.Quality < 50) )
+                {
+                    if (item.SellIn < 6)
+                    {
+                        item.Quality += 2;
+                    }
+                    else if (item.SellIn < 11)
+                    {
+                        item.Quality += 1;                                
+                    }                            
+                }                
+
+                /***********************/
+                
                 if (item.Name != "Sulfuras, Hand of Ragnaros")
                 {
                     item.SellIn -= 1;
